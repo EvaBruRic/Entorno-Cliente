d4884aa45793b39b31bd18dd2fd1aadb2417254a
Eva <evabruric@alu.edu.gva.es>
2026-09-18 20:38:53 +0200

    Actividad 6 para terminaren casa

---
+++ actividad6/script.js
@@ -0,0 +1,11 @@
+/*Haz un programa para que el usuario juegue a adivinar un número.
+
+Obtén un número al azar (busca por internet cómo se hace o simplemente guarda el número que quieras en una variable) y ve pidiendo al usuario que introduzca un número.
+
+Si es el que busca le dices que lo ha encontrado y si no le mostrarás si el número que busca el mayor o menor que el introducido.
+
+El juego acaba cuando el usuario encuentra el número o cuando pulsa en Cancelar (en ese caso le mostraremos un mensaje de que ha cancelado el juego). */
+
+let num1 = parseInt(Math.random() + 100);
+
+console.log(num1);
