76002066bb574fa07bb02799805d9a91a4b24ebf
Eva Bruno Rico <evabruric@alu.edu.gva.es>
2026-09-18 17:24:52 +0200

    Añadiendo actividad 4 y un ejemplo del temario

---
+++ Ej Template literals/script.js
@@ -0,0 +1,3 @@
+let num = 123;
+
+console.log(`El numero ${num} es el introducido`);
---
+++ actividad4/script.js
@@ -0,0 +1,6 @@
+function escribe(){
+    let texto = prompt("Escribe algo");
+    alert(`Has escrito ${texto}`);
+}
+
+console.log(escribe());
